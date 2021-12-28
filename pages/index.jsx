621c736473042ee4d9bdad3ced73e5258abd38ec
Chester Cabahug <chesterdeveloper2000@gmail.com>
2021-12-28 10:34:53 +0800

    learned about next.js 's < Head> component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,12 @@
+import Head from "next/head";
+
 function Homepage() {
   return (
     <>
+    <Head>
+      <title>My Blog</title>
+      <meta name="description" value="This is my blog"/>
+    </Head>
       <main>
         <h1>Chester's blog</h1>
       </main>
