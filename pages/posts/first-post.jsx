c650bbc2b289a504490111459d4ee241b853c825
Chester Cabahug <chesterdeveloper2000@gmail.com>
2021-12-31 10:13:47 +0800

    4>5 learned about outputting a local json file into a page in nextjs

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,27 +1,25 @@
-import Head from "next/head"
+import { readFile } from "fs/promises";
+import Head from "next/head";
 
 export async function getStaticProps() {
-  console.log("[FirstPostPage] getStaticProps()")
+  console.log("[FirstPostPage] getStaticProps()");
+  const data = await readFile("content/posts/first-post.json", "utf-8");
+  const post = JSON.parse(data);
   return {
-    props: {
-      post: {
-        title: "First Post!!",
-        body: "My first post, as static props"
-      }
-    }
-  }
+    props: { post },
+  };
 }
 
-function FirstPostPage({post}) {
-  console.log("[FirstPostPage] render:", post)
+function FirstPostPage({ post }) {
+  console.log("[FirstPostPage] render:", post);
   return (
     <>
-    <Head>
-      <title>{post.title}</title>
-    </Head>
-    <h1>{post.body}</h1>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+      <h1>{post.body}</h1>
     </>
-  )
+  );
 }
 
-export default FirstPostPage
+export default FirstPostPage;
